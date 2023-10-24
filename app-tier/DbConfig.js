fb3b8d2aa631586094ea53334642d426417956b9
idris <yusuffidris97@gmail.com>
2023-10-24 12:52:11 +0100

    docker

---
+++ app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.ccq1bsgjd8yu.us-west-2.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'yusufs97',
+    DB_DATABASE : 'cloud337'
+});
---
+++ script.groovy
@@ -0,0 +1,18 @@
+def buildfile() {
+    echo "building the application..."
+    sh 'npm install'
+} 
+
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker_psw', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build --file=app-tier/Dockerfile -t yusidris/app-tier:2.0 .'
+        sh 'docker build --file=web-tier/Dockerfile -t yusidris/webtier:2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push yusidris/app-tier:1.0'
+        sh 'docker push yusidris/webtier:1.0'
+
+    }
+} 
+
+return this
